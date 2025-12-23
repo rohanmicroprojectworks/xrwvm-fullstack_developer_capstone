6e97edcb604f7368137a338e7388519752d99142
rohanrogers <rohanrogers5@gmail.com>
2025-12-23 23:10:38 +0530

    feat: complete capstone implementation with premium UI

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,9 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://127.0.0.1:8000']
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -61,7 +62,11 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -90,7 +95,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
@@ -134,5 +140,8 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = []
-
+STATICFILES_DIRS = [
+                        os.path.join(BASE_DIR, 'frontend/static'),
+                        os.path.join(BASE_DIR, 'frontend/build'),
+                        os.path.join(BASE_DIR, 'frontend/build/static'),
+                    ]
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,19 @@
 import LoginPanel from "./components/Login/Login"
-import { Routes, Route } from "react-router-dom";
+import Register from "./components/Register/Register"
+import Dealers from "./components/Dealers/Dealers"
+import Dealer from "./components/Dealers/Dealer"
+import PostReview from "./components/Dealers/PostReview"
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dealers" element={<Dealers />} />
+      <Route path="/dealer/:id" element={<Dealer />} />
+      <Route path="/postreview/:id" element={<PostReview />} />
+      <Route path="/" element={<Navigate to="/dealers" replace />} />
     </Routes>
   );
 }
